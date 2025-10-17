08ff244991bd3adcb0699427c137bc4f75f8a7fb
Ryno575 <Copperythread99@gmail.com>
2025-10-17 15:05:55 -0600

    Refactor binary to decimal conversion logic

--- binaryToNumber.py
+++ binaryToNumber.py
@@ -2,11 +2,9 @@
 
 def toNum(binaryString):                 ### Turns the number from binary to decimal
     total = 0
-    for i in range(len(binaryString) + 1):
-        if i == 0:
-            continue
-        if (int(binaryString[-i]) == 1):
-            total += 2**(i-1)
+    for i in range(1, len(binaryString) + 1):   ### Goes through the length of string starting at
+        if (int(binaryString[-i]) == 1):        ### the end of the string and for every '1' that it finds
+            total += 2**(i-1)                   ### the total will increment by 2^(i-1)
     return total
 
 def validNum(binaryString):               ### Makes sure that there is only 1s and 0s in the string
@@ -20,13 +18,13 @@ def validNum(binaryString):               ### Makes sure that there is only 1s a
 
 while True:
     binaryString = str(input("Enter a binary number using 0s and 1s: "))
-    validity = validNum(binaryString)
-    if validity == True:
+    validity = validNum(binaryString)    ### Checks to see if the user only put in 1s and 0s
+    if validity == True:                 ### If the user only put in 1s and 0s -> turns the binary string into decimal form
         break
     else:
-        print("Please make sure you only use 0s and 1s!")
+        print("Please make sure you only use 0s and 1s!") 
         print("")
 
 print("")
-print("Binary Number: " + binaryString)
-print("Decimal Number: " + str(toNum(binaryString)))
+print("Binary Number: " + binaryString)                 ### EXAMPLE: 10000011
+print("Decimal Number: " + str(toNum(binaryString)))    ### EXAMPLE: 131
